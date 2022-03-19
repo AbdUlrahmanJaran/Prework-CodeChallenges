214de9ffe4e97af6cf07f361dc6b3610c9db060d
AbdUlrahmanJaran <abdulrahmanjaran@gmail.com>
2022-03-19 19:27:18 +0300

    Array Max Result

---
+++ Prework-CodeChallenges/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Prework_CodeChallenges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to My Game");
+            ArrayMaxResult();
+        }
+
+        static void ArrayMaxResult()
+        {
+            int [] array = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Enter a Number between 1-10: "+ (i+1));
+                String userInput1 = Console.ReadLine();
+                int userInput1I = int.Parse(userInput1);
+                array[i] = userInput1I;
+            }
+            Console.WriteLine();
+            Console.Write("Pick a number from: [");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i != array.Length - 1) Console.Write(",");
+                if (i == array.Length - 1) Console.Write("]");
+            }
+            Console.WriteLine();            
+            String userInput2 = Console.ReadLine();
+            int userInput2I = int.Parse(userInput2);
+            Console.WriteLine();
+            Score(array, userInput2I);
+        }
+
+        static void Score(int [] array , int userInput2I)
+        {
+            int score = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == userInput2I) score += userInput2I;
+            }
+            Console.WriteLine("Score : " + score);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Prework-CodeChallenges
+## Array Max Result
+1. Run my code.
+2. Enter 5 Numbers between 1-10.
+3. Enter a Number from the Array.
+4. Score: Your Number * frequency.
+
+<br>
+
+![Array Max Result](./images/ArrayMaxResult.png)
